284b439369d558cd7577dc945d2960872e89717d
RuiAlonso <rui.alonso@verygood.ventures>
2023-11-20 16:09:21 +0100

    feat: include authentication at app

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,6 +1,18 @@
+import 'package:authentication_repository/authentication_repository.dart';
 import 'package:dash_ai_search/app/app.dart';
 import 'package:dash_ai_search/bootstrap.dart';
 
 void main() {
-  bootstrap(() => const App());
+  bootstrap(() async {
+    final authenticationRepository = AuthenticationRepository(
+      firebaseAuth: firebaseAuth,
+    );
+
+    await authenticationRepository.signInAnonymously();
+    await authenticationRepository.idToken.first;
+
+    return App(
+      user: authenticationRepository.user.first,
+    );
+  });
 }
