4ccfe26cbe887787e0e42dd3b3333cc78ae73f35
RuiAlonso <rui.alonso@verygood.ventures>
2023-11-21 11:01:26 +0100

    test: app tests with mocked user

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -1,10 +1,13 @@
 import 'package:api_client/api_client.dart';
+import 'package:authentication_repository/authentication_repository.dart';
 import 'package:dash_ai_search/app/app.dart';
 import 'package:dash_ai_search/counter/counter.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 
 class _MockApiClient extends Mock implements ApiClient {}
+
+class _MockUser extends Mock implements User {}
 
 void main() {
   group('App', () {
@@ -18,6 +21,7 @@ void main() {
       await tester.pumpWidget(
         App(
           apiClient: apiClient,
+          user: _MockUser(),
         ),
       );
       expect(find.byType(CounterPage), findsOneWidget);
