34ada5d52da0109bcfd358710de0d37c823a7b30
Oscar <martinm.oscar@gmail.com>
2023-11-22 14:58:01 +0100

    chore: tests

--- packages/api_client/test/src/resources/questions_resource_test.dart
+++ packages/api_client/test/src/resources/questions_resource_test.dart
@@ -31,27 +31,51 @@ void main() {
     });
 
     group('getVertexResponse', () {
-      test('returns a VertexResponse for api enabled', () async {
-        when(() => response.statusCode).thenReturn(200);
-        final vertexResponse = VertexResponse(
-          summary: '',
-          documents: const [
-            VertexDocument(
-              metadata: VertexMetadata(url: '', title: '', description: ''),
-            ),
-          ],
-        );
-        when(() => response.body).thenReturn(
-          jsonEncode(vertexResponse.toJson()),
-        );
-        final result = await questionsResourceApiEnabled.getVertexResponse('');
-        expect(result, equals(vertexResponse));
+      group('realApiEnabled=true', () {
+        test('returns a VertexResponse if success', () async {
+          when(() => response.statusCode).thenReturn(200);
+          final vertexResponse = VertexResponse(
+            summary: '',
+            documents: const [
+              VertexDocument(
+                metadata: VertexMetadata(url: '', title: '', description: ''),
+              ),
+            ],
+          );
+          when(() => response.body).thenReturn(
+            jsonEncode(vertexResponse.toJson()),
+          );
+          final result =
+              await questionsResourceApiEnabled.getVertexResponse('');
+          expect(result, equals(vertexResponse));
+        });
+
+        test('throws ApiClientError when request fails', () async {
+          when(() => response.statusCode).thenReturn(500);
+          when(() => response.body).thenReturn('Ops');
+          await expectLater(
+            questionsResourceApiEnabled.getVertexResponse(''),
+            throwsA(isA<ApiClientError>()),
+          );
+        });
+
+        test('throws ApiClientError when request success but wrong body',
+            () async {
+          when(() => response.statusCode).thenReturn(200);
+          when(() => response.body).thenReturn('Ops');
+          await expectLater(
+            questionsResourceApiEnabled.getVertexResponse(''),
+            throwsA(isA<ApiClientError>()),
+          );
+        });
       });
 
-      test('returns a VertexResponse for api disabled', () async {
-        final result =
-            await questionsResourceApiNotEnabled.getVertexResponse('');
-        expect(result, isA<VertexResponse>());
+      group('realApiEnabled=false', () {
+        test('returns a VertexResponse', () async {
+          final result =
+              await questionsResourceApiNotEnabled.getVertexResponse('');
+          expect(result, isA<VertexResponse>());
+        });
       });
     });
   });
