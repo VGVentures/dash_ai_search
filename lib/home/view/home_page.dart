cf240adc1ddcb4700f948d04955c5db59c40cf3e
RuiAlonso <rui.alonso@verygood.ventures>
2023-11-22 10:25:48 +0100

    feat: added dash animation

--- lib/home/view/home_page.dart
+++ lib/home/view/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:app_ui/app_ui.dart';
 import 'package:dash_ai_search/home/home.dart';
 import 'package:flutter/material.dart';
 
@@ -21,7 +22,39 @@ class HomePage extends StatelessWidget {
             child: Logo(),
           ),
           InitialScreen(),
+          Positioned(
+            bottom: 50,
+            left: 50,
+            child: DashAnimation(),
+          ),
         ],
+      ),
+    );
+  }
+}
+
+class DashAnimation extends StatelessWidget {
+  @visibleForTesting
+  const DashAnimation({super.key});
+
+  static const dashSize = Size(800, 800);
+
+  @override
+  Widget build(BuildContext context) {
+    final screenSize = MediaQuery.sizeOf(context);
+
+    return Container(
+      alignment: Alignment.center,
+      height: screenSize.height / 3,
+      width: screenSize.height / 3,
+      child: const AnimatedSprite(
+        showLoadingIndicator: false,
+        sprites: Sprites(
+          asset: 'dash_animation.png',
+          size: dashSize,
+          frames: 34,
+          stepTime: 0.07,
+        ),
       ),
     );
   }
