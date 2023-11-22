ac810e7ca499f875cbadf9e203c4355b2ae42c17
Oscar <martinm.oscar@gmail.com>
2023-11-22 16:53:29 +0100

    chore: analyzer

--- packages/questions_repository/test/src/questions_repository_test.dart
+++ packages/questions_repository/test/src/questions_repository_test.dart
@@ -1,4 +1,3 @@
-// ignore_for_file: prefer_const_constructors
 import 'package:api_client/api_client.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:questions_repository/questions_repository.dart';
