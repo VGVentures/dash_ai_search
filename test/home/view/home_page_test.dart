e110eb0241b42220c795b56f2b388e0dcc6a79d8
RuiAlonso <rui.alonso@verygood.ventures>
2023-11-23 12:34:04 +0100

    test: tests coverage

--- test/home/view/home_page_test.dart
+++ test/home/view/home_page_test.dart
@@ -1,18 +1,44 @@
+import 'package:bloc_test/bloc_test.dart';
 import 'package:dash_ai_search/home/home.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
 
 import '../../helpers/helpers.dart';
 
+class _MockHomeBloc extends MockBloc<HomeEvent, HomeState>
+    implements HomeBloc {}
+
 void main() {
   group('HomePage', () {
-    testWidgets('renders correctly', (tester) async {
+    testWidgets('renders HomeView', (tester) async {
       await tester.pumpApp(
-        HomeView(),
+        HomePage(),
       );
 
-      expect(find.byType(Background), findsOneWidget);
-      expect(find.byType(Logo), findsOneWidget);
-      expect(find.byType(WelcomeView), findsOneWidget);
+      expect(find.byType(HomeView), findsOneWidget);
+    });
+
+    group('HomeView', () {
+      late HomeBloc homeBloc;
+
+      setUp(() {
+        homeBloc = _MockHomeBloc();
+        when(() => homeBloc.state).thenReturn(HomeState.initial());
+      });
+
+      testWidgets('renders correctly', (tester) async {
+        await tester.pumpApp(
+          BlocProvider.value(
+            value: homeBloc,
+            child: HomeView(),
+          ),
+        );
+
+        expect(find.byType(Background), findsOneWidget);
+        expect(find.byType(Logo), findsOneWidget);
+        expect(find.byType(WelcomeView), findsOneWidget);
+      });
     });
   });
 }
