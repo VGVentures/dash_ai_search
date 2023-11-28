cad8f1b88d6cf7c372502e01687dec15eb0d42be
Erick <erickzanardoo@gmail.com>
2023-11-28 11:50:56 -0300

    feat: adding Dash animation (#24)

--- lib/home/widgets/widgets.dart
+++ lib/home/widgets/widgets.dart
@@ -1,5 +1,6 @@
 export 'background.dart';
 export 'circle.dart';
+export 'dash_animation_container.dart';
 export 'logo.dart';
 export 'question_view.dart';
 export 'results_view.dart';
---
+++ test/home/widgets/dash_animation_container_test.dart
@@ -0,0 +1,76 @@
+import 'dart:async';
+
+import 'package:bloc_test/bloc_test.dart';
+import 'package:dash_ai_search/home/home.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:phased/phased.dart';
+
+import '../../helpers/helpers.dart';
+
+class _MockHomeBloc extends MockBloc<HomeEvent, HomeState>
+    implements HomeBloc {}
+
+void main() {
+  group('DashAnimationContainer', () {
+    late HomeBloc homeBloc;
+
+    setUp(() {
+      homeBloc = _MockHomeBloc();
+
+      whenListen(
+        homeBloc,
+        Stream.fromIterable(const <HomeState>[]),
+        initialState: const HomeState(),
+      );
+    });
+
+    Widget bootstrap([PhasedState<DashAnimationPhase>? animationState]) =>
+        BlocProvider.value(
+          value: homeBloc,
+          child: DashAnimationContainer(
+            animationState: animationState,
+          ),
+        );
+
+    testWidgets('renders correctly', (tester) async {
+      await tester.pumpApp(bootstrap());
+
+      expect(find.byType(DashAnimation), findsOneWidget);
+    });
+
+    testWidgets('updates the animation state correctly', (tester) async {
+      final state = PhasedState<DashAnimationPhase>(
+        values: DashAnimationPhase.values,
+        initialValue: DashAnimationPhase.initial,
+      );
+
+      final animationController = StreamController<HomeState>();
+      whenListen(
+        homeBloc,
+        animationController.stream,
+        initialState: const HomeState(),
+      );
+
+      expect(state.value, equals(DashAnimationPhase.initial));
+      await tester.pumpApp(bootstrap(state));
+
+      expect(state.value, equals(DashAnimationPhase.dashIn));
+
+      animationController.add(
+        const HomeState(status: Status.askQuestionToThinking),
+      );
+      await tester.pump();
+
+      expect(state.value, equals(DashAnimationPhase.dashOut));
+
+      animationController.add(
+        const HomeState(status: Status.thinkingToResults),
+      );
+      await tester.pump();
+
+      expect(state.value, equals(DashAnimationPhase.dashIn));
+    });
+  });
+}
