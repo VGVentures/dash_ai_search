1d228162d7754df7a82cd474987a0061471620c7
Oscar <martinm.oscar@gmail.com>
2023-12-01 00:25:49 +0100

    chore: wip

--- packages/app_ui/lib/src/widgets/primary_cta.dart
+++ packages/app_ui/lib/src/widgets/primary_cta.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 /// A button that displays an image on the left side and a text on the right
 /// side.
 /// {@endtemplate}
-class PrimaryCTA extends StatelessWidget {
+class PrimaryCTA extends StatefulWidget {
   /// {@macro primary_cta}
   const PrimaryCTA({
     required this.label,
@@ -24,33 +24,86 @@ class PrimaryCTA extends StatelessWidget {
   final VoidCallback? onPressed;
 
   @override
+  State<PrimaryCTA> createState() => _PrimaryCTAState();
+}
+
+class _PrimaryCTAState extends State<PrimaryCTA>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller = AnimationController(
+    vsync: this,
+    duration: const Duration(milliseconds: 200),
+  );
+  late Animation<double> _width;
+  late Animation<double> _opacity;
+  late Animation<double> _opacity2;
+  late Animation<Offset> _offset;
+  @override
+  void initState() {
+    super.initState();
+    _width = Tween<double>(begin: 50, end: 100).animate(_controller);
+    _opacity = Tween<double>(begin: 1, end: 0).animate(_controller);
+    _opacity2 = Tween<double>(begin: 0, end: 1).animate(_controller);
+    _offset = Tween<Offset>(begin: const Offset(-100, 0), end: Offset.zero)
+        .animate(_controller);
+  }
+
+  @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      onPressed: onPressed,
-      style: ElevatedButton.styleFrom(
-        padding: EdgeInsets.only(
-          left: icon != null ? 8 : 32,
-          top: 20,
-          bottom: 20,
-          right: 32,
-        ),
-      ),
-      child: Row(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          if (icon != null)
-            Padding(
-              padding: const EdgeInsets.only(right: 16),
-              child: CircleAvatar(
-                backgroundColor: VertexColors.white,
-                child: SizedBox.square(dimension: 24, child: icon),
+      onPressed: widget.onPressed,
+      onHover: (hovered) {
+        if (hovered) {
+          _controller.forward(from: 0);
+        } else {
+          _controller.reverse(from: 1);
+        }
+      },
+      child: SizedBox(
+        height: 50,
+        width: 100,
+        child: Stack(
+          children: [
+            Align(
+              alignment: Alignment.centerLeft,
+              child: AnimatedBuilder(
+                animation: _controller,
+                builder: (_, __) {
+                  return Opacity(
+                    opacity: _opacity.value,
+                    child: Container(
+                      width: _width.value,
+                      decoration: const BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(24)),
+                        color: VertexColors.white,
+                      ),
+                    ),
+                  );
+                },
               ),
             ),
-          Text(
-            label,
-            textAlign: TextAlign.center,
-          ),
-        ],
+            Align(
+              alignment: Alignment.centerLeft,
+              child: AnimatedBuilder(
+                animation: _controller,
+                builder: (_, __) {
+                  return Transform.translate(
+                    offset: _offset.value,
+                    child: Opacity(
+                      opacity: _opacity2.value,
+                      child: Container(
+                        width: 50,
+                        decoration: const BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(24)),
+                          color: VertexColors.white,
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
