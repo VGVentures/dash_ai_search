42f3c64add0e785097b11c2618dd9440a4bfdb8a
RuiAlonso <rui.alonso@verygood.ventures>
2023-12-11 16:26:34 +0100

    refactor: searchbox to question folder

--- lib/home/widgets/widgets.dart
+++ lib/home/widgets/widgets.dart
@@ -1,5 +1,4 @@
 export 'circle.dart';
 export 'dash_animation_container.dart';
 export 'logo.dart';
-export 'search_box.dart';
 export 'transition_screen_mixin.dart';
---
+++ lib/question/widgets/widgets.dart
@@ -0,0 +1,1 @@
+export 'search_box.dart';
